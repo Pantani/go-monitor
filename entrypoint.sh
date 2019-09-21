40720ca06ca9a268a18a5931dc8e7ecbc30b932a
Danilo Pantani <danpantani@gmail.com>
2019-09-21 15:59:30 -0300

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+prometheusconfig=`/bin/cat /prometheus/prometheus.yml 2>/dev/null | wc -l`
+if [[ "$prometheusconfig" -gt "0" ]]; then
+
+    echo "Changing the app url: " $APP_URL
+    /bin/cat /prometheus/prometheus.yml > /etc/prometheus/prometheus.yml
+    sed -i -e 's|{{.APP_URL}}|'$APP_URL'|g' /etc/prometheus/prometheus.yml
+
+fi
+
+echo "****** Runing Prometheus ******"
+/bin/prometheus --config.file=/etc/prometheus/prometheus.yml
